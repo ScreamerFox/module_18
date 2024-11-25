466a23d8ab5a316d18e2b26778379ba73e0f55f5
Screfox <142076193+ScreamerFox@users.noreply.github.com>
2024-11-25 15:32:00 +0500

    Update views.py

--- module_18_3/task3/views.py
+++ module_18_3/task3/views.py
@@ -5,25 +5,49 @@ from .models import Image
 
 def main(request):
     title = 'Tripleware'
+    h1_p = 'Главная страница'
+    first = 'Главная'
+    second = 'Продукты разработчика'
+    third = 'Корзина'
     context = {
         'title': title,
+        'h1_p': h1_p,
+        'first': first,
+        'second': second,
+        'third': third,
     }
     return render(request, 'main_page.html', context)
 
 
-def second(requests):
-    title = 'Продукты разработчика'
-    img = 'images/backg_kf.jpeg'
+def second_page(requests):
+    title = 'Products'
+    h1_p = 'Продукты разработчика'
+    first = 'Killing floor'
+    second = 'Killing floor 2'
+    third = 'Killing floor: Incursion'
+    fourth = 'Killing floor 3'
+    main_p = 'На главную'
     context = {
         'title': title,
+        'h1_p': h1_p,
+        'first': first,
+        'second': second,
+        'third': third,
+        'fourth': fourth,
+        'main_p': main_p,
     }
     return render(requests, 'second_page.html', context)
 
 
-def third(requests):
-    title = 'Корзина'
+def third_page(requests):
+    title = 'Basket'
+    h1_p = 'Ваши покупки:'
+    main_p = 'На главную'
+    if_none = 'Кажется здесь пусто :)'
     context = {
+        'h1_p': h1_p,
+        'main_p': main_p,
+        'if_none': if_none,
         'title': title,
     }
     return render(requests, 'third_page.html', context)
-
