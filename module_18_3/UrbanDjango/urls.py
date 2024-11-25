f799c55f89755a48a3759155ebbf8c89000bd963
Screfox <142076193+ScreamerFox@users.noreply.github.com>
2024-11-25 15:33:03 +0500

    Update urls.py

--- module_18_3/UrbanDjango/urls.py
+++ module_18_3/UrbanDjango/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from task3.views import main, second, third
+from task3 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main),
-    path('second/', second),
-    path('third/', third),
+    path('', views.main),
+    path('second/', views.second_page),
+    path('third/', views.third_page),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
